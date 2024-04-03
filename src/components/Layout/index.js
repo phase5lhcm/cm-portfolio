e536a73d4a7bc5a103a50c039b150a87232b51a9
cmaynard <chrissy.swe.maynard@gmail.com>
2024-04-02 20:05:35 -0400

    First commit; setup

---
+++ src/components/Layout/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Layout = () => {
+  return (
+    <div style={{ color: 'white', background: 'red' }}>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default Layout
